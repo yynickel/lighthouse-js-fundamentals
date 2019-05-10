fe28e8132b2eae16f55e47669e0e3e2589641661
Nick Yu <yynickel@gmail.com>
2019-05-10 23:26:21 +0000

    event.js exercise with 'isEven' function completed

---
+++ even.js
@@ -0,0 +1,12 @@
+function isEven(num) {
+    return num % 2 === 0;
+}
+
+// var tenIsEven = isEven(10);
+// var elevenIsEven = isEven(11);
+
+// console.log(tenIsEven);
+// console.log(elevenIsEven);
+
+console.log(isEven(10));
+console.log(isEven(11));
